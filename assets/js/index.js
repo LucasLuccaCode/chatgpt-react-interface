7f3016cceeef4e73b7a691cdf6faa3900d381133
Lucca Santos <lucasluccad.r@outlook.com>
2023-02-15 00:08:37 -0300

    feat(settings): add settings for application control

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,24 @@
-const c_responses = document.querySelector('.c-responses')
+const root = document.documentElement
+const menu = document.querySelector('[data-menu]')
+const blur = document.querySelector('[data-settings-blur]')
+const responses = document.querySelector('.c-responses')
 const placeholder = document.querySelector('.placeholder')
 const progressElem = document.querySelector(".c-status__progress")
 const stopBtn = document.querySelector(".c-status__stop")
 const form = document.querySelector('.c-form')
 const questionEntry = document.querySelector('.c-form textarea')
 
+const settingsForm = document.querySelector('[data-settings]')
+const inputsSettings = settingsForm.querySelectorAll('input')
+const temperatureValue = document.querySelector('[data-temperature-value]')
+const closeBtn = document.querySelector('[data-settings-close]')
+
 let currentQuestion = null
 let currentAnswer = null
 let questionsAndAnswers = []
+let settings = {}
 
 const API_KEY = "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"
-const renderingSpeed = 40
-const maxTokens = 2048
-const temperature = 0.6
 let isPrinting = false
 let controller = null
 
@@ -35,7 +41,7 @@ const renderLoadedData = () => {
     return acc
   }, [])
 
-  c_responses.append(...tags)
+  responses.append(...tags)
 }
 
 const loadDataStorage = () => {
@@ -45,11 +51,47 @@ const loadDataStorage = () => {
   if (data.length) {
     renderLoadedData()
   } else {
-    c_responses.innerHTML = '<p class="placeholder">Faça uma pergunta para exibir aqui a resposta...</p>'
+    responses.innerHTML = '<p class="placeholder">Faça uma pergunta para exibir aqui a resposta...</p>'
   }
-  c_responses.scrollTop = c_responses.scrollHeight;
+  responses.scrollTop = responses.scrollHeight;
 }
 loadDataStorage()
+
+const initialSettings = {
+  font_size: 18,
+  max_tokens: 2050,
+  rendering_speed: 40,
+  temperature: 0.6,
+  save_context,
+  save_queries
+}
+
+const loadSettingsStorage = () => {
+  settings = JSON.parse(localStorage.getItem("@mr:chatGPT:settings")) || initialSettings
+
+  const keys = Object.keys(settings)
+  keys.forEach(key => {
+    const isCheckbox = typeof settings[key] === 'boolean'
+    if (isCheckbox) {
+      document.getElementById(key).checked = settings[key]
+      return
+    }
+
+    // number and radio input
+    const value = settings[key]
+    document.getElementById(key).value = value
+
+    if (key === 'temperature') {
+      const temperature = value
+      temperatureValue.textContent = temperature.toFixed(1)
+    }
+
+    if (key === 'font_size') {
+      root.style.setProperty('--font-size', `${value}px`)
+    }
+  })
+}
+loadSettingsStorage()
 
 const saveDataStorage = () => {
   const dataJson = JSON.stringify(questionsAndAnswers)
@@ -61,9 +103,9 @@ const sendQuestion = async (question) => {
   const h2 = createElement('h2', 'question', question)
   h2.innerHTML = '<img src="./assets/img/send.png" alt="Ícone para enviar pergunta"></img>' + h2.textContent
 
-  c_responses.appendChild(h2)
+  responses.appendChild(h2)
 
-  c_responses.scrollTop = c_responses.scrollHeight;
+  responses.scrollTop = responses.scrollHeight;
 
   stopBtn.classList.remove('hide')
   progressElem.textContent = "Aguardando resposta da api..."
@@ -74,7 +116,7 @@ const sendQuestion = async (question) => {
 
   if (jsonResponse.error?.message) {
     const h2 = createElement('h2', 'response', jsonResponse.error.message)
-    c_responses.appendChild(h2)
+    responses.appendChild(h2)
     return
   }
 
@@ -110,8 +152,8 @@ const fetchAPI = async (question) => {
       body: JSON.stringify({
         model: "text-davinci-003",
         prompt: question,
-        max_tokens: maxTokens, // tamanho da resposta
-        temperature: temperature, // criatividade na resposta
+        max_tokens: settings.max_tokens, // tamanho da resposta
+        temperature: settings.temperature, // criatividade na resposta
       }),
       signal
     })
@@ -136,7 +178,7 @@ const writeText = async (text) => {
   text = text.replace(/^(\n)+/g, '');
 
   const responseElem = createElement('pre', 'response', "Chat GPT:\n\n")
-  c_responses.appendChild(responseElem)
+  responses.appendChild(responseElem)
 
   isPrinting = true
   for (let i = 0; i < text.length; i++) {
@@ -146,14 +188,14 @@ const writeText = async (text) => {
     progressElem.textContent =
       `Respondendo [ ${i + 1} / ${text.length}  ] ${Math.floor((i + 1) / text.length * 100)}%`;
 
-    c_responses.scrollTop = c_responses.scrollHeight;
+    responses.scrollTop = responses.scrollHeight;
 
     if (!isPrinting) {
       progressElem.textContent = "Renderização parada..."
       return
     }
 
-    await sleep(renderingSpeed)
+    await sleep(settings.rendering_speed)
   }
 
   isPrinting = false
@@ -162,6 +204,12 @@ const writeText = async (text) => {
 
   stopBtn.classList.add('hide')
 }
+
+const showHideBlurSettings = () => {
+  blur.classList.toggle('active')
+}
+
+menu.addEventListener('click', showHideBlurSettings)
 
 const handleForm = (e) => {
   e.preventDefault()
@@ -186,3 +234,73 @@ const handleStopRequest = () => {
 }
 
 stopBtn.addEventListener('click', handleStopRequest)
+
+const handleSettingsForm = (e) => {
+  e.preventDefault()
+
+  const formData = new FormData(e.target)
+  const {
+    font_size,
+    max_tokens,
+    rendering_speed,
+    temperature,
+    save_context,
+    save_queries
+  } = Object.fromEntries(formData)
+
+  const updatedData = {
+    font_size: Number(font_size),
+    max_tokens: Number(max_tokens),
+    rendering_speed: Number(rendering_speed),
+    temperature: Number(temperature),
+    save_context: !!save_context,
+    save_queries: !!save_queries
+  }
+
+  settings = updatedData
+
+  const settingsJson = JSON.stringify(updatedData)
+
+  localStorage.setItem("@mr:chatGPT:settings", settingsJson)
+
+  showHideBlurSettings()
+}
+
+
+settingsForm.addEventListener('submit', handleSettingsForm)
+
+const handleInputsSettings = ({ target: el }) => {
+  const name = el.getAttribute('name')
+
+  const allowedSettings = {
+    font_size: () => {
+      const size = el.value
+      root.style.setProperty('--font-size', `${size}px`)
+    },
+    temperature: () => {
+      const newValue = Number(el.value)
+      temperatureValue.textContent = newValue.toFixed(1)
+    },
+    save_context: () => {
+      el.checked && console.log('O contexto das do chat será enviado nas requisições futuras.')
+    },
+    save_queries: () => {
+      el.checked && console.log('As consultas serão salvas.')
+    }
+  }
+
+  const fn = allowedSettings[name]
+  fn && fn()
+}
+
+inputsSettings.forEach(input =>
+  input.addEventListener('change', handleInputsSettings))
+
+const handleCloseClick = (e) => {
+  e.preventDefault()
+
+  loadSettingsStorage()
+  showHideBlurSettings()
+}
+
+closeBtn.addEventListener('click', handleCloseClick);
