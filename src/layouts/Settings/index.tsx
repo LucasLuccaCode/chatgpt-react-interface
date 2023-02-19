35139bcd0e3791ca968851ebba2be97d25e9afa3
Lucca Santos <lucasluccad.r@outlook.com>
2023-02-19 01:24:39 -0300

    feat: create settings component

--- src/layouts/Settings/index.tsx
+++ src/layouts/Settings/index.tsx
@@ -1,10 +1,165 @@
-import React from "react"
-import { Container } from "./styles"
+import React, { ChangeEvent, useCallback, useState } from "react"
+import {
+  SettingsContainer,
+  Form,
+  Division,
+  Wrapper,
+  Control,
+  Buttons,
+  Button,
+  Label,
+  InputRange,
+  InputToggle,
+} from "./styles"
+
+interface ISettings {
+  size: number,
+  temperature: number,
+  tokens: number,
+  speed: number,
+  darkTheme: boolean,
+  queries: boolean,
+  contexts: boolean
+}
+
+interface MyObject {
+  [key: string]: number | boolean;
+}
+
+const initialSettings: ISettings = {
+  size: 18,
+  temperature: 0.6,
+  tokens: 2050,
+  speed: 40,
+  darkTheme: true,
+  queries: true,
+  contexts: false
+}
 
 export const Settings: React.FC = () => {
+  const [settings, setSettings] = useState<ISettings>(initialSettings)
+
+  const handleInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target
+    const name: unknown = input.getAttribute('name')
+
+    if(typeof name !== 'string') return
+
+    const inputsCheckbox = ['darkTheme', 'contexts', 'queries']
+
+    let obj: MyObject;
+
+    const isCheckbox = inputsCheckbox.includes(name)
+    obj = isCheckbox ? { [name]: input.checked } : { [name]: Number(input.value) }
+
+    setSettings(prevState => ({ ...prevState, ...obj }))
+  }, [])
+
   return (
-    <Container>
-      <h2>Settings</h2>
-    </Container>
+    <SettingsContainer>
+      <Form>
+        <Division>
+          <Wrapper>
+            <Label>Tamanho dos textos</Label>
+            <Control>
+              <InputRange
+                type="range"
+                min="14"
+                max="30"
+                step="1"
+                value={settings.size}
+                name="size"
+                onChange={handleInputChange}
+              />
+              <span>{settings.size}</span>
+            </Control>
+          </Wrapper>
+          <Wrapper>
+            <Label>Temperatura</Label>
+            <Control>
+              <InputRange
+                type="range"
+                min="0"
+                max="1"
+                step="0.1"
+                value={settings.temperature}
+                name="temperature"
+                onChange={handleInputChange}
+              />
+              <span>{settings.temperature}</span>
+            </Control>
+          </Wrapper>
+          <Wrapper>
+            <Label>Máximo de tokens</Label>
+            <Control>
+              <InputRange
+                type="range"
+                min="1"
+                max="4001"
+                step="100"
+                value={settings.tokens}
+                name="tokens"
+                onChange={handleInputChange}
+              />
+              <span>{settings.tokens}</span>
+            </Control>
+          </Wrapper>
+          <Wrapper>
+            <Label>Velocidade de renderização(ms)</Label>
+            <Control>
+              <InputRange
+                type="range"
+                max="1000"
+                min="0"
+                step="1"
+                value={settings.speed}
+                name="speed"
+                onChange={handleInputChange}
+              />
+              <span>{settings.speed}</span>
+            </Control>
+          </Wrapper>
+        </Division>
+
+        <Division>
+          <Control>
+            <Label htmlFor="darkTheme">Tema dark</Label>
+            <InputToggle
+              type="checkbox"
+              name="darkTheme"
+              id="darkTheme"
+              onChange={handleInputChange}
+            />
+          </Control>
+          <Control>
+            <Label htmlFor="contexts">Ativar contexto</Label>
+            <InputToggle
+              type="checkbox"
+              name="contexts"
+              id="contexts"
+              onChange={handleInputChange}
+            />
+          </Control>
+          <Control>
+            <Label htmlFor="queries">Salvar consultas</Label>
+            <InputToggle
+              type="checkbox"
+              name="queries"
+              id="queries"
+              onChange={handleInputChange}
+            />
+          </Control>
+        </Division>
+
+        <Buttons>
+          {/* <Button>
+            Reset
+          </Button> */}
+          <Button type="submit">
+            Salvar
+          </Button>
+        </Buttons>
+      </Form>
+    </SettingsContainer>
   )
 }
--- src/layouts/Settings/styles.ts
+++ src/layouts/Settings/styles.ts
@@ -1,8 +1,169 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const SettingsContainer = styled.section`
 grid-area: settings;
-background: #050505;
-border: 1px solid rgba(255, 255, 255, .05);
 border-radius: ${props => props.theme.b_radius};
+padding: .5rem;
+/* background: rgba(255, 255, 255, .05); */
+overflow: hidden;
+`
+
+export const Form = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+gap: 1rem;
+height: 100%;
+`
+
+export const Division = styled.div`
+display: flex;
+flex-direction: column;
+gap: 1rem;
+`
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+gap: .5rem;
+width: 100%;
+`
+export const Control = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 100%;
+gap: .5rem;
+
+  input {
+    flex: 1;
+  }
+
+  span {
+    font-size: .8rem;
+    color: #8a8a90;
+    margin-left: .5rem;
+  }
+`
+
+export const Label = styled.label`
+color: #aaaab0;
+margin-right: 1rem;
+font-size: .8rem;
+white-space: nowrap;
+text-overflow: ellipsis;
+overflow: hidden;
+`
+
+export const InputCheckbox = styled.input`
+display: flex;
+align-items: center;
+justify-content: center;
+position: relative;
+appearance: none;
+width: 1rem;
+height: 1rem;
+border-radius: .2rem;
+background: rgba(0, 0, 0, 1);
+
+  &:checked::before {
+    content: "";
+    position: absolute;
+    border-left: .18rem solid ${props => props.theme.colors.details};
+    border-bottom: .18rem solid ${props => props.theme.colors.details};
+    display: inline-block;
+    width: .4rem;
+    height: .3rem;
+    margin-top: -.2rem;
+    transform: rotate(-45deg);
+  }
+`
+
+export const InputRange = styled.input`
+-webkit-appearance: none;
+-moz-appearance: none;
+appearance: none;
+width: 100%;
+height: .4rem;
+background-color: rgba(255, 255, 255, .1);
+border-radius: 5px;
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: .7rem;
+    height: .7rem;
+    background-color: ${props => props.theme.colors.details};
+    border-radius: 50%;
+  }
+
+  &::-moz-range-thumb {
+    appearance: none;
+    width: .7rem;
+    height: .7rem;
+    background-color: ${props => props.theme.colors.details};
+    border-radius: 50%;
+  }
+`
+
+export const InputToggle = styled.input`
+--width: 1.6rem;
+--height: .8rem;
+-webkit-appearance: none;
+-moz-appearance: none;
+appearance: none;
+position: relative;
+display: inline-block;
+width: var(--width);
+max-width: var(--width);
+height: .8rem;
+background: rgba(255, 255, 255, .1);
+border-radius: 50px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: var(--height);
+    height: var(--height);
+    background-color: ${props => props.theme.colors.text};
+    border-radius: 50%;
+    transition: all 0.3s;
+  }
+
+  &:checked::before {
+    transform: translateX(100%);
+    background: ${props => props.theme.colors.details};
+  }
+`
+
+export const Buttons = styled.div`
+display: flex;
+gap: .5rem;
+align-items: center;
+`
+
+export const Button = styled.button`
+padding: .4rem .5rem;
+flex: 1;
+margin-top: 1rem;
+font-size: .6rem;
+font-weight: bold;
+color: #efefff;
+border-radius: .2rem;
+background: rgba(255, 255, 255, .1);
+opacity: .9;
+
+  &:last-child {
+    color: ${props => props.theme.colors.secondary};
+    background: ${props => props.theme.colors.details};
+  }
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &:active {
+    transform: scale(.95)
+  }
 `
