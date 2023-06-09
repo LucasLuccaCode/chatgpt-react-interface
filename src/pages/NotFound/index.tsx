a5f87318170ff9713ad05a506a72b0de6413faca
Lucca Santos <lucasluccad.r@outlook.com>
2023-06-09 10:26:50 -0300

    style: change page style NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,17 +1,22 @@
 import { useNavigate, useRouteError } from "react-router-dom"
-import { NotFoundStyled } from "./styles"
 
-export const NotFound: React.FC = () => {
+import { Button, Container, Content } from "./styles"
+
+const NotFound: React.FC = () => {
   const error: any = useRouteError()
   const navigate = useNavigate()
 
   const handleButton = () => navigate(-1)
 
   return (
-    <NotFoundStyled>
-      <h1>{error.status}</h1>
-      <h2>{error.statusText}</h2>
-      <button className="btn" onClick={handleButton}>Voltar</button>
-    </NotFoundStyled>
+    <Container>
+      <Content>
+        <h1>{error.status || 500}</h1>
+        <h2>{error.statusText || error.message}</h2>
+      </Content>
+      <Button onClick={handleButton}>Voltar</Button>
+    </Container>
   )
 }
+
+export default NotFound
--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -1,17 +1,44 @@
 import styled from "styled-components"
 
-export const NotFoundStyled = styled.div`
-  display: grid;
-  place-content: center;
-  gap: .6rem;
-  width: 100%;
-  height: 100%;
-  background: red;
+export const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+`
 
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* gap: .6rem; */
+  padding: 1rem;
+  border-radius: .5rem;
+  background: #3a3a40;
+  
+  
   h1,
   h2 {
     font-size: 1.1rem;
     color: #efefff;
     text-align: center;
+  }
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  all: unset;
+  text-align: center;
+  color: #efefff;
+  font-weight: bold;
+  margin-top: 1rem;
+  padding: .6rem;
+  border-radius: .3rem;
+  transition: all .3s ease;
+  background: #5768ef;
+
+  &:hover {
+    background-color: #4758df;
   }
 `
